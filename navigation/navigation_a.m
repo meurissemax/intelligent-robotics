64e185a6e6962cb6a645716dd57380ebc11530e1
Maxime Meurisse <M.Meurisse@student.uliege.be>
2020-03-17 10:30:48 +0100

    File organisation

--- navigation/navigation_a.m
+++ navigation/navigation_a.m
@@ -1,3 +1,7 @@
+% INFO0948-2 - Introduction to intelligent robotics
+% University of Liege - Academic year 2019-2020
+% Authors : Maxime Meurisse & Valentin Vermeylen
+
 function youbot()
     % youbot Illustrates the V-REP Matlab bindings.
 
